7be647140719f41cd45df57f95de48ddb78a5b32
lucas-aquino350 <lucasaquino350@gmail.com>
2024-01-14 17:30:59 -0300

    implementa teste web

--- pessoa-endereco/src/main/java/br/com/attornatus/pessoaendereco/PessoaEnderecoApplication.java
+++ pessoa-endereco/src/main/java/br/com/attornatus/pessoaendereco/PessoaEnderecoApplication.java
@@ -2,12 +2,21 @@ package br.com.attornatus.pessoaendereco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class PessoaEnderecoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PessoaEnderecoApplication.class, args);
-	}
+    @GetMapping
+    public String getTeste(){
+        return "TESTE PESSOA-ENDERECO";
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(PessoaEnderecoApplication.class, args);
+    }
 }
