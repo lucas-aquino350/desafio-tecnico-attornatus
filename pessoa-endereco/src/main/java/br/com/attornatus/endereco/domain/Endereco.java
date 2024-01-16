1d7e9853063cd8c53725365228f3d1f0f3846865
lucas-aquino350 <lucasaquino350@gmail.com>
2024-01-16 16:02:33 -0300

    implementa regra de negocio e relação entre endereco e pessoa

--- pessoa-endereco/src/main/java/br/com/attornatus/endereco/domain/Endereco.java
+++ pessoa-endereco/src/main/java/br/com/attornatus/endereco/domain/Endereco.java
@@ -1,4 +1,35 @@
 package br.com.attornatus.endereco.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import net.bytebuddy.implementation.bind.MethodDelegationBinder;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
+
+@Getter
+@ToString
+@Entity
+@Table(name = "endereco")
+@NoArgsConstructor
 public class Endereco {
+
+    @Id
+    @Column(name = "id", updatable = false, unique = true, nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idEndereco;
+    @NotBlank
+    @Column(unique = true)
+    private String logradouro;
+    @NotBlank
+    private String numero;
+    @NotBlank
+    private String cidade;
+    @NotNull
+    private String cep;
+    @NotBlank
+    private TipoEndereco tipoEndereco;
 }
--- pessoa-endereco/src/main/java/br/com/attornatus/pessoa/domain/Pessoa.java
+++ pessoa-endereco/src/main/java/br/com/attornatus/pessoa/domain/Pessoa.java
@@ -5,13 +5,13 @@ import br.com.attornatus.pessoa.application.api.PessoaRequest;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 
@@ -23,16 +23,17 @@ import java.util.UUID;
 public class Pessoa {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", updatable = false, unique = true, nullable = false)
     private UUID idPessoa;
     @NotBlank
     private String nome;
     @NotNull
     private LocalDate dataNascimento;
-
+    @OneToMany(mappedBy = "pessoa", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Endereco> enderecos = new ArrayList<>();
 
     public Pessoa(PessoaRequest pessoaRequest) {
-        this.idPessoa = UUID.randomUUID();
         this.nome = pessoaRequest.getNome();
         this.dataNascimento = pessoaRequest.getDataNascimento();
     }
