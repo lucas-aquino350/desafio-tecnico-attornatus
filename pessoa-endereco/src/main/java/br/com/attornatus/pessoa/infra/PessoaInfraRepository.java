99b5f18f5813c6983d72071efe2b67782f1f26c0
lucas-aquino350 <lucasaquino350@gmail.com>
2024-01-17 21:19:28 -0300

    implementa get por id camada repository

--- pessoa-endereco/src/main/java/br/com/attornatus/pessoa/infra/PessoaInfraRepository.java
+++ pessoa-endereco/src/main/java/br/com/attornatus/pessoa/infra/PessoaInfraRepository.java
@@ -8,6 +8,8 @@ import lombok.extern.log4j.Log4j2;
 import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
 @Log4j2
 @RequiredArgsConstructor
@@ -19,6 +21,15 @@ public class PessoaInfraRepository implements PessoaRepository {
         log.info("[inicia] PessoaInfraRepository - salva");
         pessoaSpringDataJPARepository.save(pessoa);
         log.info("[finaliza] PessoaInfraRepository - salva");
+        return pessoa;
+    }
+
+    @Override
+    public Pessoa buscaPessoaPorId(UUID idPessoa) {
+        log.info("[inicia] PessoaInfraRepository - buscaPessoaPorId");
+        Pessoa pessoa = pessoaSpringDataJPARepository.findById(idPessoa)
+                .orElseThrow( () -> new RuntimeException("Pessoa não encontrada!"));
+        log.info("[finaliza] PessoaInfraRepository - buscaPessoaPorId");
         return pessoa;
     }
 }
